ae3353f441115956b5c746f7e86b055749152952
Jared S <jaredjames2020@gmail.com>
2019-11-29 21:54:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
+    if req.path=="/items/#{item.name}"
  
     @@songs.each do |song|
       resp.write "#{song.title}\n"
