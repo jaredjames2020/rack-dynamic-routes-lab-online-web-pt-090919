d1235404b1b0779ddb88738f7155df869d134283
Jared S <jaredjames2020@gmail.com>
2019-11-29 21:52:47 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+ 
+  @@songs = [Song.new("Sorry", "Justin Bieber"),
+            Song.new("Hello","Adele")]
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    @@songs.each do |song|
+      resp.write "#{song.title}\n"
+    end
+ 
+    resp.finish
+  end
+end
