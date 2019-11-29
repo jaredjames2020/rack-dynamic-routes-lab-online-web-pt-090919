49dcffa69d523c728d0defec5834a1ce13cd45b3
Jared S <jaredjames2020@gmail.com>
2019-11-29 22:11:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,15 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
+    if req.path.match(/items/)
+      item_name = req.path.split("/").last
+      item = @@items.detect{|i| i.name == item_name}
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+    
     if req.path=="/items/"
       search_term = req.params["item"]
         if @@items.include?(search_term)
