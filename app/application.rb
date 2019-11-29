e0ce8b7e8b353a6ddbc7b25aff5685b4a4f2dd59
Jared S <jaredjames2020@gmail.com>
2019-11-29 22:22:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,8 +17,9 @@ class Application
     if req.path.match(/items/)
       binding.pry
       item_request = req.path.split("/").last
-        if @@items.detect{|i| i.name == item_request}
+        if @@items.detect{|i| i.name == item_request do
           resp.write "#{i.price}"
+        end
         else
         resp.write "Route not found"
         resp.status = 400
