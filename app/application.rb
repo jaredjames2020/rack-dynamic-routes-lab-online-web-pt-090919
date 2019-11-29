25c451041985873d7a682e84b5d6044c86d8b53c
Jared S <jaredjames2020@gmail.com>
2019-11-29 21:55:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,13 @@
 class Application
+  
+  @@items = []
  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if req.path=="/items/#{item.name}"
-      resp.write "#{item.name}"
+    if req.path=="/items/#{items.name}"
+      resp.write "#{items.name}"
     else
       resp.write "Route not found"
       resp.status = 404
